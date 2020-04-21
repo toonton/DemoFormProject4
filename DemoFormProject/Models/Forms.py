c5e892aee1f0224118de8ed8203a0cd820607a21
Matan Monblat <matan.monblat@gmail.com>
2020-04-21 21:25:28 +0300

    a

---
+++ DemoFormProject/Models/Forms.py
@@ -0,0 +1,6 @@
+
+from datetime import datetime
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms import Form , BooleanField , PasswordField , HiddenField , DateTimeField , IntegerField , DecimalField , FloatField , RadioField 
